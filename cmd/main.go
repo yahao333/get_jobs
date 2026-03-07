fc63b583f743c393032db8069833580929b0629c
yanghao <fexeak@163.com>
2026-03-07 23:02:00 +0800

    feat: 实现Web服务器启动逻辑
    移除原有的TODO注释，添加web包依赖并初始化Web服务器。从配置中读取端口号，若未配置则使用默认端口8080。启动服务器并在失败时输出错误信息并退出程序。

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/yahao333/get_jobs/internal/config"
 	"github.com/yahao333/get_jobs/internal/storage"
+	"github.com/yahao333/get_jobs/internal/web"
 )
 
 func main() {
@@ -24,10 +25,21 @@ func main() {
 	}
 
 	config.Info("应用启动成功!")
-	config.Info("Web 服务地址: http://", config.GetString("web.host"), ":", config.GetInt("web.port"))
 
-	// TODO: 启动 Web 服务
-	// TODO: 启动求职任务
+	// 启动 Web 服务
+	port := config.GetInt("web.port")
+	if port == 0 {
+		port = 8080
+	}
+
+	config.Info("Web 服务地址: http://localhost:", port)
+
+	// 启动 Web 服务器
+	server := web.NewServer(port)
+	if err := server.Start(); err != nil {
+		config.Error("Web 服务启动失败: ", err)
+		os.Exit(1)
+	}
 
 	// 保持运行
 	select {}
